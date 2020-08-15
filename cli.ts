a7b97849480f2c94eae8592fef2213a3e7a5b5ae
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-15 11:08:36 +0530

    feat(cli): parse CLI args

--- cli.ts
+++ cli.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
-import { checkLinks, Entry } from "./index.js";
+import sade from "sade";
+import { checkLinks, Entry, Options } from "./index.js";
+import { readFileSync } from "fs";
+
+import { DirectNavigationOptions } from "puppeteer";
 
 const typeHeading = {
 	samePage: "Same page links (fragments)",
@@ -7,24 +11,75 @@ const typeHeading = {
 	offSite: "External links",
 };
 
-async function main() {
-	const url = new URL(process.argv[2]);
+interface CommandLineOptions {
+	"same-page": boolean;
+	"same-site": boolean;
+	"off-site": boolean;
+	fragments: boolean;
+	timeout: number;
+	"wait-until": DirectNavigationOptions["waitUntil"];
+	format: "json" | "pretty";
+	emoji: boolean;
+}
+
+sade("hyperlinkinator <url>", true)
+	.version(JSON.parse(readFileSync("./package.json", "utf-8")).version)
+	.option("--same-page", "Check same-page (fragment) links", true)
+	.option("--same-site", "Check same-site links", true)
+	.option("--off-site", "Check external links", true)
+	.option(
+		"--fragments",
+		"Check for existence of IDs corresponding to fragments",
+		true,
+	)
+	.option("--timeout", "Timeout (in seconds) for navigation", 20)
+	.option(
+		"--wait-until",
+		'Wait until either "load", "domcontentloaded", "networkidle0", "networkidle2" events.',
+		"load",
+	)
+	.option("--format", "Format output as pretty or json", "pretty")
+	.option("--emoji", "Use emoji in output (with --format=pretty)", true)
+	.action(async (url: string, options: CommandLineOptions) => {
+		try {
+			await main(new URL(url), {
+				options: {
+					samePage: options["same-page"],
+					sameSite: options["same-site"],
+					offSite: options["off-site"],
+					fragments: options.fragments,
+					puppeteer: {
+						timeout: options.timeout * 1000,
+						waitUntil: options["wait-until"],
+					},
+				},
+				emoji: options["emoji"],
+				format: options.format || "pretty",
+			});
+		} catch (error) {
+			console.error(error.message);
+			process.exit(1);
+		}
+	})
+	.parse(process.argv);
+
+interface Opts {
+	options: Options;
+	emoji: boolean;
+	format: CommandLineOptions["format"];
+}
+async function main(url: URL, options: Opts) {
 	console.log(`Navigating to ${url} ...`);
 	let lastType: undefined | keyof typeof typeHeading;
-	for await (const result of checkLinks(url)) {
+	for await (const result of checkLinks(url, options.options)) {
 		if (result.type !== lastType) {
 			lastType = result.type;
 			printHeading(lastType);
 		}
-		const output = formatOutput(result, { emoji: true });
+		const output = formatOutput(result, { emoji: options.emoji });
 		console.log(output);
 	}
 }
-
-main().catch(error => {
-	console.error(error.message);
-	process.exit(1);
-});
 
 function printHeading(type: keyof typeof typeHeading) {
 	const heading = `${typeHeading[type]}:`;
